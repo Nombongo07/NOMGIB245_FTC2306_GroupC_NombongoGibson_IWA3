e864ca8d34c7665520c3250536c81e228904d52b
Nombongo07 <137196106+Nombongo07@users.noreply.github.com>
2023-08-03 13:18:55 +0200

    Add files via upload

---
+++ IWA3/challenge2/alex.js
@@ -0,0 +1,7 @@
+const firstname = "Alex"
+const surname = "Naidoo"
+export let alexRole = "Head of Marketing"
+
+const alexDetails = `${firstname} ${surname}: ${alexRole}`;
+
+document.querySelector('#alex').innerHTML = alexDetails;
